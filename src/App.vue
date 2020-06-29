5a9b4750b6e2dfd4d2f4a2b78da1390489f752da
Robert King <robertkng104@gmail.com>
2020-06-29 01:16:03 +0100

    all gucci

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="app">
+  <div id="app" tabindex=-1>
     <h1> Awesome Pool</h1>
-    <svg width=700 height=400>
+    <svg width=700 height=400  >
       <pool-table :balls=balls :x="0" :y="0" :size="700" :ballRadius="ballRadius"/>
     </svg>
     <div> 
@@ -35,26 +35,10 @@ function Ball(x,y,vx,vy,type){
   this.vx = vx;
   this.vy = vy;
   this.type = type;
-  this.impulseX = [];
-  this.impulseY = []; 
-  this.collided = [this];
+  this.collided = []; 
 }
 Ball.prototype.stationary = function(){
   return Math.abs(this.vx) < 0.22 && Math.abs(this.vy) < 0.22;
-}
-Ball.prototype.addCollided = function(collided){
-  this.collided.push(collided);
-}
-Ball.prototype.reset = function(){
-  this.collided = [this];
-  this.impulseX = [];
-  this.impulseY = [];
-}
-Ball.prototype.ax = function(){
-  return (this.impulseX.length > 0)? this.impulseX.reduce((acc,val) => acc + val)/this.impulseX.length : 0;
-}
-Ball.prototype.ay = function(){
-  return (this.impulseY.length > 0)? this.impulseY.reduce((acc,val) => acc + val)/this.impulseY.length : 0;
 }
 
 import PoolTable from "./components/PoolTable.vue"
@@ -65,15 +49,19 @@ export default {
   },
   mounted(){
     this.physicsSimulate();
+    window.addEventListener("keydown",this.pause);
+  },
+  destroyed(){
+    window.removeEventListener("keydown",this.pause);
   },
   data(){
     return{
       intervalId:null,
       balls:[
-        new Ball(-40,0,80,0,"cue"),
-        //new Ball(40,0,0,0,"red"),
-        new Ball(47.5,-4.5,0,0,"red"),
-        new Ball(47.5,4.5,0,0,"yellow")
+        new Ball(-15,0,100,0,"cue"),
+        new Ball,47.5,-4.5,0,0,"red"),
+        new Ball(47.5,4.5,0,0,"yellow"),
+        new Ball()
       ],
       restitutionCoeff:0.92,
       staticFriction: 10,
@@ -82,40 +70,53 @@ export default {
     }
   },
   methods:{
+    pause(e){
+      if(e.code === "Space"){
+        if(this.intervalId != null){
+          clearInterval(this.intervalId);
+          this.intervalId = null;
+        }else{
+          this.physicsSimulate();
+        }
+      }
+    },
     physicsSimulate(){
       let frameCount = 0; 
       this.intervalId = setInterval(function(){
-        if(this.balls.filter( x=> !x.stationary()).length == 0){ clearInterval(this.intervalId); this.intervalId = null; console.log(frameCount)}
-        for(let ball of this.balls.filter(x=>!x.stationary())){
+        let col =false;
+        if(this.balls.filter( x=> !x.stationary()).length == 0){
+           clearInterval(this.intervalId);
+            this.intervalId = null;
+             console.log(frameCount)
+        }
+        for(let ball of this.balls){
           // Check if intersecting a pocket
           checkPocket(ball.x,ball.y);
           // Check if intersecting another ball
-          for(let otherBall of this.balls.filter(x => !ball.collided.includes(x))){
-              let distance = Math.sqrt(Math.pow(otherBall.x - ball.x,2) + Math.pow(otherBall.y - ball.y,2));
-              if(distance < 2*this.ballRadius){ 
-                  let Vx = (ball.vx + otherBall.vx)/2;
-                  let Vy = (ball.vy + otherBall.vy)/2;
-                  let initAngle = Math.atan2(Vy,Vx);
-                  let b = Math.sqrt(1 - Math.pow((otherBall.x - ball.x)*Vx + Vy*(otherBall.y - ball.y),2)/(distance*distance*(Math.pow(Vx,2) + Math.pow(Vy,2))));
-                  let impactAngle = Math.asin(b) * Math.sign(otherBall.y - ball.y);
-                  let u1x = ball.vx - Vx;
-                  let u2x = otherBall.vx - Vx;
-                  let u1y = ball.vy - Vy;
-                  let u2y = otherBall.vy - Vy;
-                  ball.impulseX.push((Vx -u1x*this.restitutionCoeff*Math.cos(impactAngle - initAngle) + u1y*this.restitutionCoeff*Math.sin(impactAngle - initAngle)) - ball.vx);
-                  otherBall.impulseX.push((Vx -u2x*this.restitutionCoeff*Math.cos(impactAngle - initAngle) + u2y*this.restitutionCoeff*Math.sin(impactAngle - initAngle)) - otherBall.vx);
-                  ball.impulseY.push((Vy - u1x*this.restitutionCoeff*Math.sin(impactAngle - initAngle) - u1y*this.restitutionCoeff*Math.cos(impactAngle - initAngle)) - ball.vy);
-                  otherBall.impulseY.push((Vy - u2x*this.restitutionCoeff*Math.sin(impactAngle - initAngle) - u2y*this.restitutionCoeff*Math.cos(impactAngle - initAngle)) - otherBall.vy);
-                  ball.addCollided(otherBall);
-                  otherBall.addCollided(ball);
-              }
+          let colList = this.balls.filter(x=> !(ball.collided.includes(x)))
+                                  .filter(x=> x !== ball)
+                                  .filter(otherBall=>Math.pow(otherBall.x - ball.x,2) + Math.pow(otherBall.y - ball.y,2) < 4*this.ballRadius*this.ballRadius)
+          for(let otherBall of colList){
+            let distance = Math.sqrt(Math.pow(otherBall.x - ball.x,2) + Math.pow(otherBall.y - ball.y,2))
+            let Vx = (ball.vx + otherBall.vx)/2;
+            let Vy = (ball.vy + otherBall.vy)/2;
+            let initAngle = Math.atan2(Vy,Vx);
+            let b = Math.sqrt(1 - Math.pow((otherBall.x - ball.x)*Vx + Vy*(otherBall.y - ball.y),2)/(distance*distance*(Math.pow(Vx,2) + Math.pow(Vy,2))));
+            let impactAngle = Math.asin(b) * Math.sign(otherBall.y - ball.y);
+            let u1x = ball.vx - Vx;
+            let u2x = otherBall.vx - Vx;
+            let u1y = ball.vy - Vy;
+            let u2y = otherBall.vy - Vy;
+            ball.vx = Vx -u1x*this.restitutionCoeff*Math.cos(impactAngle - initAngle) + u1y*this.restitutionCoeff*Math.sin(impactAngle - initAngle);
+            otherBall.vx = Vx -u2x*this.restitutionCoeff*Math.cos(impactAngle - initAngle) + u2y*this.restitutionCoeff*Math.sin(impactAngle - initAngle);
+            ball.vy = Vy - u1x*this.restitutionCoeff*Math.sin(impactAngle - initAngle) - u1y*this.restitutionCoeff*Math.cos(impactAngle - initAngle);
+            otherBall.vy = Vy - u2x*this.restitutionCoeff*Math.sin(impactAngle - initAngle) - u2y*this.restitutionCoeff*Math.cos(impactAngle - initAngle);
+            ball.collided.push(otherBall);
+            otherBall.collided.push(ball);
           }
         }
-        for(let ball of this.balls){
-          // Reflect at edge...
-          let aX = ball.ax();
-          let aY = ball.ay();
-          ball.reset();
+       
+        for(let ball of this.balls.filter(x=>!x.stationary())){
           if(Math.abs(ball.x) > 100) { 
             ball.vx = -ball.vx* this.restitutionCoeff;
             ball.vy = ball.vy * this.restitutionCoeff;
@@ -127,9 +128,8 @@ export default {
             ball.y = Math.sign(ball.y)*50;
           }
           let angle = Math.atan2(ball.vy,ball.vx);
-          ball.vx -= ((this.frictionCoeff)*ball.vx + this.staticFriction)*Math.cos(angle)*(1/60) - aX;
-          ball.vy -= ((this.frictionCoeff)*ball.vy + this.staticFriction)*Math.sin(angle)*(1/60) - aY;  
-
+          ball.vx -= ((this.frictionCoeff)*ball.vx + this.staticFriction)*Math.cos(angle)*(1/60);
+          ball.vy -= ((this.frictionCoeff)*ball.vy + this.staticFriction)*Math.sin(angle)*(1/60); 
           // Update position
           ball.x += ball.vx * (1/60);
           ball.y += ball.vy * (1/60);
@@ -138,9 +138,13 @@ export default {
             ball.vx = 0;
             ball.vy = 0;
           }
-        }  
-        frameCount++;
-
+        }
+        for(let ball of this.balls) {
+          ball.collided = ball.collided
+                                  .filter(otherBall=>Math.pow(otherBall.x - ball.x,2) + Math.pow(otherBall.y - ball.y,2) < 4*this.ballRadius*this.ballRadius);
+       }
+      if(col) console.log(this.balls[0].collided);
+      frameCount++;
       }.bind(this),1000/60); // 60FPS
     }
   },
